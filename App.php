b12390fae175be7bdfb49934feb9e178a8cb1130
Raymond Fung <ray@uberws.com>
2022-07-13 22:14:04 -0400

    main app class init db with CRUD template functions

---
+++ App.php
@@ -0,0 +1,35 @@
+<?
+class App {
+    private $db_pdo;
+
+    function __construct($host, $db, $user, $pass, $charset) {
+
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+        ];
+
+        try {
+            $this->db_pdo = new PDO($dsn, $user, $pass, $options);
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
+
+    public function addBook() {
+
+    }
+
+    public function viewBook() {
+
+    }
+
+    public function updateBook() {
+
+    }
+
+    pubic function deleteBook() {
+
+    }
+}
